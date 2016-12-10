160eacfeddd3ba1e6ec48cfcf8aa75a4066e2707
getgoh <blujaygo@gmail.com>
2016-12-10 14:06:49 -0500

    Added tax page

---
+++ Tax.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Dorknozzle.master" AutoEventWireup="true" CodeFile="Tax.aspx.cs" Inherits="Tax" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <h1>Generate Tax</h1>
+
+    <h3>Employee Information</h3>
+    <span class="widelabel2">Employee: </span>
+    <asp:Label id="txtEmployeeName" runat="server" />
+    <br />
+    <span class="widelabel2">Total Income: </span>
+    <asp:Label id="txtEmployeeSalary" runat="server" />
+    <br />
+    <span class="widelabel2">Taxes Paid: </span>
+    <asp:Label id="txtTaxesPaid" runat="server" />
+    <br />
+    <span class="widelabel2">Contribution: </span>
+    <asp:Label id="txtContribution" runat="server" />
+    <br /><br />
+    <asp:Button Text="Submit" ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" />
+
+</asp:Content>
+
---
+++ Tax.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tax : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        User currUser;
+        if (Session["User"] == null)
+        {
+            Session["RedirectPage"] = "Tax.aspx";
+            Response.Redirect("Login.aspx");
+        }
+
+        if (!IsPostBack)
+        {
+            currUser = (User)Session["User"];
+            //computeInterest();
+            txtEmployeeName.Text = currUser.Firstname + " " + currUser.Lastname;
+            txtEmployeeSalary.Text = currUser.TotalIncome.ToString("C", CultureInfo.GetCultureInfo("en-US"));
+
+            txtTaxesPaid.Text = currUser.TotalTaxesPaid.ToString("C", CultureInfo.GetCultureInfo("en-US"));
+            txtContribution.Text = currUser.Contributions.ToString("C", CultureInfo.GetCultureInfo("en-US"));
+        }
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        // security measure when idle for a long time.. 
+        if (Session["User"] == null)
+        {
+            Session["RedirectPage"] = "RequestLoan.aspx";
+            Response.Redirect("Login.aspx");
+        }
+    }
+
+    }
